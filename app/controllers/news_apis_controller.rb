cff05ee62c28060c20032467d88e5f480ed75b38
Billiesnyc <billiesnyc@gmail.com>
2018-11-09 13:23:02 +0000

    added requests from api

--- app/controllers/news_apis_controller.rb
+++ app/controllers/news_apis_controller.rb
@@ -9,15 +9,13 @@ class NewsApisController < ApplicationController
     # TEST:  /top-headlines?country=us  
 
     
-    def query
+    def query(type, query, sort)
       base_url = "https://newsapi.org/v2"
-      api_key = "#{ENV['NEWSKEY']}"
-      puts api_key
-      request = HTTParty.get(base_url+"/top-headlines?country=us&apiKey="+api_key).to_json
+      request = HTTParty.get(base_url + '/' + type + '?q=' + query + '&sortBy=' + sort + '&apiKey=' + ENV['NEWSKEY']).to_json
       @request_hash = JSON.parse(request)
     end 
 
-    render json: query
+    render json: query('everything', 'bitcoin', 'popularity')
     end
 
 end 
