9300c41b94f662d63cfee13dd8f8bba3c322aa64
braj011 <bhagyaraj01@gmail.com>
2018-11-09 16:01:22 +0000

    Do not commit

--- app/controllers/news_apis_controller.rb
+++ app/controllers/news_apis_controller.rb
@@ -8,7 +8,6 @@ class NewsApisController < ApplicationController
     
     # TEST:  /top-headlines?country=us  
 
-    
     def query(type, query, sort)
       base_url = "https://newsapi.org/v2"
       request = HTTParty.get(base_url + '/' + type + '?q=' + query + '&sortBy=' + sort + '&apiKey=' + ENV['NEWSKEY']).to_json
