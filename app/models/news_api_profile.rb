0fa765146748055166e1f2a8a4df528310052416
Billiesnyc <billiesnyc@gmail.com>
2018-11-12 14:45:29 +0000

    added user page

---
+++ app/models/news_api_profile.rb
@@ -0,0 +1,3 @@
+class NewsApiProfile < ApplicationRecord
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :categories
+  resources :users
   resources :news_apis, only: [:create]
-  post '/news_apis', to: 'news_apis#query'
+  resources :news_apis_profile, only: [:create]
+  post '/news_apis_profile/', to: 'news_apis_profile#query'
 end
